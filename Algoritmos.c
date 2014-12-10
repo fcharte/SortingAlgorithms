14c6fc0207549c562a5cfe9c4f344c95f7cf6be2
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-10 17:46:48 +0100

    Bubble sort implementation

--- Algoritmos.c
+++ Algoritmos.c
@@ -2,3 +2,23 @@
 #include "Algoritmos.h"
 
 /* Sorting function implementation */
+
+/* Bubble sort */
+void ordBurbuja(int elementos[], int nelementos, Informa f, TEstado* estado)
+{
+  int i, j, temp;
+  boolean fin=false;
+
+  for (i = (nelementos - 1); i >= 0 && !fin; i--)
+    for (j = 1; j <= i && !fin; j++) {
+      if (elementos[j-1] > elementos[j]) {
+        temp = elementos[j-1];
+        elementos[j-1] = elementos[j];
+        elementos[j] = temp;
+        estado->deElemento=j;
+        estado->aElemento=j-1;
+        fin=f(estado);
+      }
+      estado->nOperaciones++;
+    }
+}
--- Algoritmos.h
+++ Algoritmos.h
@@ -3,4 +3,6 @@
 
 /* Sorting function prototypes */
 
+void ordBurbuja(int elementos[], int nelementos, Informa f, TEstado* estado); /* Bubble sort */
+
 #endif /* __ALGORITMOS_H__ */
