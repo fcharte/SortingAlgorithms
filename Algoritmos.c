7b74454c0896d4d1478730dec8f00d327a31a983
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-11 09:05:31 +0100

    Improved bubble sort implementation

--- Algoritmos.c
+++ Algoritmos.c
@@ -22,3 +22,27 @@ void ordBurbuja(int elementos[], int nelementos, Informa f, TEstado* estado)
       estado->nOperaciones++;
     }
 }
+
+/* Improved bubble sort */
+void ordBurbujaMejorado(int elementos[], int nelementos, Informa f, TEstado* estado)
+{
+  int i=1, j, temp;
+  boolean fin=false, hayCambio=true;
+
+  while(i<nelementos && hayCambio && !fin) {
+    hayCambio=false;
+    for (j = 0; j <= nelementos-i && !fin; j++) {
+      if (elementos[j] > elementos[j+1]) {
+        temp = elementos[j];
+        elementos[j] = elementos[j+1];
+        elementos[j+1] = temp;
+        estado->deElemento=j;
+        estado->aElemento=j+1;
+        fin=f(estado);
+        hayCambio=true;
+      }
+      estado->nOperaciones++;
+    }
+    i++;
+   }
+}
--- Algoritmos.h
+++ Algoritmos.h
@@ -4,5 +4,6 @@
 /* Sorting function prototypes */
 
 void ordBurbuja(int elementos[], int nelementos, Informa f, TEstado* estado); /* Bubble sort */
+void ordBurbujaMejorado(int elementos[], int nelementos, Informa f, TEstado* estado); /* Improved bubble sort */
 
 #endif /* __ALGORITMOS_H__ */
