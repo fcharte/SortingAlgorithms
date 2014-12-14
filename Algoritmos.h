1bcc6e5713d37648c7d61051a89ef817b91832d9
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-14 11:15:34 +0100

    Shell sort algorithm

--- Algoritmos.h
+++ Algoritmos.h
@@ -69,5 +69,17 @@ void ordInsercion(int elementos[], int nelementos, Informa f, TEstado* estado); 
 */
 void ordSacudida(int elementos[], int nelementos, Informa f, TEstado* estado); /* Shaker sort (also known as cocktail sort) */
 
+/**
+   @brief Ordena un vector de enteros mediante el algoritmo Shell
+   @param elementos Vector con los elementos a ordenar
+   @param nelementos Número de elementos que contiene el vector
+   @param f Puntero a la función de notificación de cambios
+   @param estado Puntero a una estructura TEstado con la información del proceso
+   @pre El vector de elementos debe contener al menos 2 elementos
+   @pre La estructura TEstado a la que apunta @a estado debe estar inicializada
+   @post El proceso de ordenación finaliza cuando el vector esté ordenado o bien cuando la función de notificación devuelva @a verdad
+*/
+void ordShell(int elementos[], int nelementos, Informa f, TEstado* estado); /* Shell sort */
+
 
 #endif /* __ALGORITMOS_H__ */
