11278e4c7cac0e710ec495ad6828c7eb47cbaa73
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-15 09:50:25 +0100

    Quick sort algorithm

--- Algoritmos.h
+++ Algoritmos.h
@@ -81,5 +81,16 @@ void ordSacudida(int elementos[], int nelementos, Informa f, TEstado* estado); /
 */
 void ordShell(int elementos[], int nelementos, Informa f, TEstado* estado); /* Shell sort */
 
+/**
+   @brief Ordena un vector de enteros mediante el algoritmo de QuickSort
+   @param elementos Vector con los elementos a ordenar
+   @param nelementos Número de elementos que contiene el vector
+   @param f Puntero a la función de notificación de cambios
+   @param estado Puntero a una estructura TEstado con la información del proceso
+   @pre El vector de elementos debe contener al menos 2 elementos
+   @pre La estructura TEstado a la que apunta @a estado debe estar inicializada
+   @post El proceso de ordenación finaliza cuando el vector esté ordenado o bien cuando la función de notificación devuelva @a verdad
+*/
+void ordQuick(int elementos[], int nelementos, Informa f, TEstado* estado); /* Quick sort */
 
 #endif /* __ALGORITMOS_H__ */
