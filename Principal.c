54ecc76921236dc55d2ef4258bf820d5bc37c8a2
Francisco Charte Ojeda <francisco@fcharte.com>
2014-12-23 08:59:53 +0100

    Added main application file

---
+++ Principal.c
@@ -0,0 +1,29 @@
+/** @mainpage Aplicación de prueba de algoritmos de ordenación
+    @e Escrita por Francisco Charte Ojeda
+
+    Esta aplicación está escrita en lenguaje C y basada en los algoritmos de
+    ordenación más documentados en la literatura. Su finalidad es didáctica,
+    permitiendo obtener una visualización gráfica del proceso de ordenación.
+    El programa puede ampliarse agregando nuevos algoritmos de ordenación y
+    registrándolos con el núcleo de la aplicación, para lo cual es necesario
+    seguir las instrucciones agregadas al código.
+
+    Este programa puede compilarse en cualquier sistema que cuente con un
+    compilador ANSI de C y la biblioteca ncurses, que se utiliza para crear
+    la interfaz de usuario. Dicha biblioteca está incluida en todas las
+    distribuciones de Linux y también forma parte de otros sistemas operativos,
+    como Mac OS X, pero en el caso de DOS/Windows es necesario añadirla al
+    compilador que esté utilizándose. La página oficial del compilador MingW,
+    que es el que incluye Dev-C++, ofrece ncurses y muchas otras bibliotecas
+    útiles para desarrollar aplicaciones.
+
+    Las tres capturas siguientes lo muestran en
+    funcionamiento en DOS, Linux y Mac OS X.
+
+    @htmlonly
+    <img src="EnDos.jpg"><hr>
+    <img src="EnLinux.jpg"><hr>
+    <img src="EnMacOSX.jpg"><hr>
+    @endhtmlonly
+*/
+
